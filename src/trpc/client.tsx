187751fe45b11aa76a5d4094469798edf41fb135
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-02-14 11:29:14 +0530

    Fix: Improved login faster

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -44,6 +44,11 @@ export function TRPCProvider(
         httpBatchLink({
           transformer: superjson, // <- - if you use a data transformer
           url: getUrl(),
+          async headers() {
+            const headers = new Headers();
+            headers.set("x-trpc-source", "nextjs-react");
+            return headers;
+          },
         }),
       ],
     }),
